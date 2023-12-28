05b313b9761eba785f8297a1cb26689348e0b5a0
hyemin-1107 <hyem92.11.07@gmail.com>
2023-12-29 05:53:26 +0900

    2023-12-29 채널톡 종합박스

--- sooldamhwa/src/Pages/Subscribe/Subscribe.js
+++ sooldamhwa/src/Pages/Subscribe/Subscribe.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import { Link } from 'react-scroll';
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -35,12 +35,23 @@ import PosterModalImg from '../../images/subscribeImage/popup_poster.png'
 import CurationModalImg from '../../images/subscribeImage/popup_curation.png'
 
 import PcKakaoImg from '../../images/subscribeImage/channel_talk_pc.png'
+import MessengerLogo from '../../images/subscribeImage/msg_logo_img.jpeg'
+import DetailLogo from '../../images/subscribeImage/msg_logo.jpeg'
+import Inquiryimg from '../../images/subscribeImage/inquiry_img.png'
+import TimeImg from '../../images/subscribeImage/app_time.png'
+import KakaoIco from '../../images/subscribeImage/app_messenger_kakao.png'
+import NaverIco from '../../images/subscribeImage/app_messenger_naver_talk.png'
+import CallIco from '../../images/subscribeImage/app_messenger_call.png'
+import ChannelIco from '../../images/subscribeImage/ch_logo.png'
 
 const Subscribe = () => {
 
     const [isModal, setIsModal] = useState(false);
+
+    const [isMonthSoolModal, setIsMonthSoolModal] = useState(false);
     const [isCurationModal, setIsCurationModal] = useState(false);
     const [isPostModal, setIsPostModal] = useState(false);
+    const [isPcKakoModal, setIsPcKakoModal] = useState(false);
 
     const onClickModal = () => {
         setIsModal(true);
@@ -50,7 +61,16 @@ const Subscribe = () => {
         setIsModal(false);
     }
 
-    
+
+    const onClickMonthSoolModal = () => {
+        setIsMonthSoolModal(true);
+    };
+
+    const onClickMonthSoolButton = () => {
+        setIsMonthSoolModal(false);
+    }
+
+
     const onClickCurationModal = () => {
         setIsCurationModal(true);
     };
@@ -69,8 +89,19 @@ const Subscribe = () => {
     }
 
 
+    const onClickPcKakoModal = () => {
+        setIsPcKakoModal(true);
+    };
 
-    const navigate = useNavigate();
+    const onClickPcKakoButton = () => {
+        setIsPcKakoModal(false);
+    }
+
+
+
+
+
+    // const navigate = useNavigate();
 
 
     return (
@@ -113,7 +144,7 @@ const Subscribe = () => {
                                 나의 전통주 취향을 알아볼 수 있어요.
                             </TotalBoxSubDescription>
 
-                            <TotalBoxButton>
+                            <TotalBoxButton onClick={onClickMonthSoolModal}>
                                 <div>
                                     <TotalBoxButtonImg src={MonthSoolImg} />
                                     <TotalBoxButtonTitle>1월의 술 3종</TotalBoxButtonTitle>
@@ -134,7 +165,7 @@ const Subscribe = () => {
                                     <SeeMoreImg src={GreyArrow} />
                                 </div>
                             </TotalBoxButton>
-                            
+
                             <TotalBoxButton onClick={onClickPosterModal}>
                                 <div>
                                     <TotalBoxButtonImg src={PosterImg} />
@@ -166,17 +197,70 @@ const Subscribe = () => {
 
             </SubscribeLandingBox>
 
-          
-
 
             <ThisMonthsConcept />
             <QnAContents />
-            <PcKakao src={PcKakaoImg} onClick={() => { navigate(" "); }} />
+            <PcKakao src={PcKakaoImg} onClick={onClickPcKakoModal} isPcKakoModal={isPcKakoModal} />
+
+            <PcKakaoModalContainer isPcKakoModal={isPcKakoModal}>
+                <PcKakaoModalHeader>
+                    <PcKakaoModalHeaderImg src={MessengerLogo} />
+                    <div>
+                        <PcKakaoModalHeaderTitle>술담화</PcKakaoModalHeaderTitle>
+                        <PcKakaoModalHeaderSub>운영시간 보기 {'>'}</PcKakaoModalHeaderSub>
+                    </div>
+                </PcKakaoModalHeader>
+                <PcKakaoDetail>
+                    <PcKakaoDetailContainer>
+                        <PcKakaoDetailImg src={DetailLogo} />
+                        <Detail>술담화
+                            <div style={{ height: '2px' }}></div>
+                            <div>안녕하세요, 고객님!</div>
+                            <div>찾아오는 인생술, 술담화입니다.</div>
+                            <div style={{ height: '1rem' }}> </div>
+                            <div>현재는 고객센터 운영시간이 아닙니다.</div>
+                            <div>성함과 연락처, 문의사항을 남겨주시면 영업 시간 내 순차적으로 안내 드리겠습니다. :)..</div>
+                        </Detail>
+                    </PcKakaoDetailContainer>
+                    <DetailInquiry>문의하기
+                        <InquiryImg src={Inquiryimg} />
+                    </DetailInquiry>
+                    <DetailTime>
+                        <DetailTimeImg src={TimeImg} />
+                        오후 1:30부터 운영해요
+                    </DetailTime>
+                </PcKakaoDetail>
+                <OtherWay>
+                    <OtherWayText>다른 방법으로 문의</OtherWayText>
+                    <KakaoImg src={KakaoIco} />
+                    <NaverImg src={NaverIco} />
+                    <CallImgContainer>
+                        <CallImg src={CallIco} />
+                    </CallImgContainer>
+                </OtherWay>
+                <ChannelTalk>
+                    <ChannelImg src={ChannelIco}/>
+                    채널톡<span>이용중</span>
+                </ChannelTalk>
+
+            </PcKakaoModalContainer>
+
+            <PcKakaoModalButton isPcKakoModal={isPcKakoModal} onClick={onClickPcKakoButton}>
+                <PcKakaoModalButtonImg src={ModalClose} />
+            </PcKakaoModalButton>
+
             <Footer />
             <PopupContent />
+
             <ApplyButton onClick={onClickModal}>지금 신청하기</ApplyButton>
             <ApplyModal isModal={isModal}
                 onClickButton={onClickButton} />
+
+            <MonthSoolModalWrap isMonthSoolModal={isMonthSoolModal}>
+                <MonthSoolModalContainer>
+                    <ModalButton onClick={onClickMonthSoolButton} src={ModalClose} />
+                </MonthSoolModalContainer>
+            </MonthSoolModalWrap>
 
             <CurationModalWrap isCurationModal={isCurationModal}>
                 <ModalContainer>
@@ -195,6 +279,7 @@ const Subscribe = () => {
         </>
     )
 };
+
 
 
 const SubscribeLandingBox = styled.div`
@@ -354,6 +439,7 @@ const TotalBoxSubDescription = styled.div`
     height: 60px;
 
     font-size: 13px;
+    font-weight: 500;
     line-height: 20px;
 
     color: rgba(61, 61, 61, 0.6);
@@ -464,23 +550,44 @@ const ApplyButton = styled.button`
     }
 `
 
-const PcKakao = styled.img`
+const MonthSoolModalWrap = styled.div`
+    display: ${(props) => (props.isMonthSoolModal ? "block" : "none")};
     position: fixed;
-    right: 50px;
-    bottom: 50px;
 
-    width: 75px;
-    height: 75px;
+    top: 0;
+    left: 0;  
+   
+    width: 100%;
+    height: 100%;
+   
+    z-index: 99;
 
-    z-index: 1 ;
-    cursor: pointer;
-
-    animation: motion 0.5s linear 0s infinite alternate;
-    @keyframes motion {
-	    0% {bottom: 50px;}
-	    100% {bottom: 60px;}
-}
+    background-color: rgba(0,0,0,0.2);
 `
+
+const MonthSoolModalContainer = styled.div`
+    position: relative;
+
+    top: 50%;
+    left: 50%;
+    z-index: 100;
+    transform: translate(-50%, -50%);
+
+    padding: 20px 20px 30px;
+
+    width: 390px;
+    height: 660px;
+
+    display: flex;
+    justify-content: center;
+
+    background: #fff;
+
+    box-shadow: 2px 2px 30px 1px rgba(0,0,0,0.5);
+    box-sizing: border-box;
+    border-radius: 20px;
+`
+
 
 const CurationModalWrap = styled.div`
     display: ${(props) => (props.isCurationModal ? "block" : "none")};
@@ -537,10 +644,10 @@ const ModalContainer = styled.div`
 const ModalButton = styled.img`
     position: absolute;
     
-    right: 30px;
+    right: 26px;
 
-    width: 32px;
-    height: 32px;
+    width: 33px;
+    height: 33px;
     
     cursor: pointer;
 `
@@ -548,5 +655,286 @@ const ModalButton = styled.img`
 const ModalImg = styled.img`
     width: 340px;
 `
+
+
+const PcKakao = styled.img`
+    display: ${(props) => (props.isPcKakoModal ? "none" : "block")};
+    position: fixed;
+    right: 50px;
+    bottom: 50px;
+
+    width: 75px;
+    height: 75px;
+
+    z-index: 1 ;
+    cursor: pointer;
+
+    animation: motion 0.5s linear 0s infinite alternate;
+    @keyframes motion {
+	    0% {bottom: 50px;}
+	    100% {bottom: 60px;}
+}
+`
+
+const PcKakaoModalButton = styled.div`
+    display: ${(props) => (props.isPcKakoModal ? "block" : "none")};
+    position: fixed;
+    right: 50px;
+    bottom: 50px;
+    z-index: 100;
+
+    box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset,
+    rgba(0, 0, 0, 0.1) 0px 4px 6px, rgba(0, 0, 0, 0.15) 0px 8px 30px;
+    background: #fff;
+    border-radius: 24px;
+    cursor: pointer;
+
+    transition: ease .3s;
+    &:hover{
+        transition: ease .3s;
+        box-shadow: rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset,
+    rgba(0, 0, 0, 0.1) 0px 0px 6px 6px, rgba(0, 0, 0, 0.15) 0px 8px 30px;
+    }
+`
+
+const PcKakaoModalButtonImg = styled.img`
+    display: block;
+    padding: 14px;
+    
+    width: 32px;
+    height: 32px;
+`
+
+const PcKakaoModalContainer = styled.div`
+    display: ${(props) => (props.isPcKakoModal ? "block" : "none")};
+    position: fixed;
+    
+    right: 50px;
+    bottom: 120px;
+
+    z-index: 1;
+
+    width: 390px;
+    height: 650px;
+
+    background: #F9F9F9;
+    border-radius: 20px;
+    box-shadow: 2px 2px 30px 1px rgba(0,0,0,0.4);
+`
+
+const PcKakaoDetail = styled.div`
+    display: flex;
+    flex-direction: column;
+    
+    gap: 12px;
+    margin: 0 16px;
+    padding: 8px;
+
+    border-radius: 20px;
+    background: #fff;
+`
+
+const PcKakaoDetailContainer = styled.div`
+    display: flex;
+
+    gap: 2px;
+
+    cursor: pointer;
+    
+    transition: ease .3s;
+    &:hover{
+        transition: ease .3s;
+        border-radius: 20px;
+        background-color: #F6F6F6;
+    }
+`
+
+const PcKakaoModalHeader = styled.div`
+    display: flex;
+    align-items: center;
+    
+    gap: 14px;
+    margin: 24px;
+`
+
+const PcKakaoModalHeaderImg = styled.img`
+    width: 56px;
+    height: 56px;
+
+    border-radius: 20px;
+`
+
+const PcKakaoModalHeaderTitle = styled.div`
+    padding-left: 3px;
+    
+    font-size: 22px;
+    font-weight: 600;
+`
+
+const PcKakaoModalHeaderSub = styled.div`
+    padding: 3px;
+
+    font-size: 13px;
+    color: #00000099;
+    
+    cursor: pointer;
+
+    transition: ease .3s;
+    &:hover{
+        transition: ease .3s;
+        border-radius: 10px;
+        background-color: #EFEFF0;
+    }
+`
+
+const PcKakaoDetailImg = styled.img`
+    margin: 8px;
+    
+    width: 36px;
+    height: 36px;
+    
+    border-radius: 50%;
+    border: 1px solid #EFEFF0;
+`
+
+const Detail = styled.div`
+    padding: 8px 0 12px;
+    width: 252px;
+
+    font-size: 14px;
+    font-weight: 600;
+
+    div{
+        font-size: 15px;
+        font-weight: 400;
+    }
+`
+
+const DetailInquiry = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    gap: 6px;
+    padding: 14px;
+    
+    width: 100%;
+
+    font-size: 15px;
+    font-weight: 600;
+
+    border: none;
+    border-radius: 20px;
+
+    color: #fff;
+    background: #0096F3;
+    
+    cursor: pointer;
+    
+    transition: 0.3s ease;
+    &:hover{
+        background: rgb(45, 123, 195);
+        transition: 0.3s ease;
+    }
+`
+
+const InquiryImg = styled.img`
+    width: 20px;
+`
+
+const DetailTime = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    gap: 4px;
+    margin-bottom: 8px;
+
+    font-size: 12px;
+    font-weight: 500;
+    
+    color: #00000099;
+`
+
+const DetailTimeImg = styled.img`
+    width: 16px;
+`
+
+const OtherWay = styled.div`
+    display: flex;
+    align-items: center;
+
+    gap: 6px;
+    margin: 14px 16px;
+    padding: 14px;
+    background: #fff;
+    border-radius: 20px;
+`
+
+const OtherWayText = styled.div`
+    width: 210px;
+    
+    font-size: 13px;
+    font-weight: 500;
+
+    color: #00000066;
+`
+
+const KakaoImg = styled.img`
+    width: 34px;
+    
+    border-radius: 10px;
+    cursor: pointer;
+`
+
+const NaverImg = styled.img`
+    width: 34px;
+
+    border-radius: 10px;
+    cursor: pointer;
+`
+
+const CallImgContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 34px;
+    height: 34px;
+    
+    border-radius: 10px;
+    background: #329BE733;
+    cursor: pointer;
+`
+
+const CallImg = styled.img`
+    width: 20px;
+    height: 20px;
+`
+
+const ChannelTalk = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 3px;
+
+    height: 30px;
+
+    font-size: 12px;
+    font-weight: 600;
+    color: #00000066;
+
+    span{
+        font-weight: 400;
+    }
+`
+
+const ChannelImg = styled.img`
+    width: 20px;
+    height: 20px;
+`
+
+
 
 export default Subscribe;
