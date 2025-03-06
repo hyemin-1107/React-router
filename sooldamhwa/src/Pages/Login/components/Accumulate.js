c6919aa55b28f68a0fe9fc534a5821a49f5bdff4
hyemin-1107 <hyem92.11.07@gmail.com>
2025-03-06 14:21:36 +0900

    edit

--- sooldamhwa/src/Pages/Login/components/Accumulate.js
+++ sooldamhwa/src/Pages/Login/components/Accumulate.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import styled from 'styled-components';
-import icon_coin from '../../../images/loginImage/icon_coin.png'
+import React from "react";
+import styled from "styled-components";
+import icon_coin from "../../../images/loginImage/icon_coin.png";
 
 const Accumulate = () => {
-
-    return(
-        <Accumulatecontent>
-        지금 회원가입하면 최대 4,000p 적립!
-        <LoginCoin src={icon_coin} />
-        </Accumulatecontent>
-    )
-
+  return (
+    <Accumulatecontent>
+      지금 회원가입하면 최대 4,000p 적립!
+      <LoginCoin src={icon_coin} />
+    </Accumulatecontent>
+  );
 };
 
 const Accumulatecontent = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    margin-top: 56px;
+  margin-top: 56px;
 
-    height: 36px;
+  height: 36px;
 
-    font-size: 13px;
-    font-weight: 700;
+  font-size: 13px;
+  font-weight: 700;
 
-    background: #5197F2;
-    color: #fff;
-`
+  background: #5197f2;
+  color: #fff;
+`;
 
 const LoginCoin = styled.img`
-    margin-left: 5px;
-    
-    width: 24px;
-    height: 24px;
-`
+  margin-left: 5px;
+
+  width: 24px;
+  height: 24px;
+`;
 
 export default Accumulate;
